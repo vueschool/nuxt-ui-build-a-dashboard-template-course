06ee8886b29362a518b57c6b17b85ed93cf63dc9
Daniel Kelly <me@danielkelly.io>
2025-10-14 17:48:58 -0500

    2-boot-up-a-nuxt-ui-project-from-a-template-and-editor-setup

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+    '@nuxt/image'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
