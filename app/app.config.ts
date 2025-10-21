2c72f198d942f9bae53f0357daf79b4a9efef44b
Daniel Kelly <me@danielkelly.io>
2025-10-20 20:43:50 -0500

    7-nuxt-ui-simple-component-variants

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,20 @@
 export default defineAppConfig({
   ui: {
+    button: {
+      defaultVariants: {
+        size: 'xl'
+      }
+    },
     card: {
       slots: {
         header: 'bg-red-500'
+      },
+      variants: {
+        variant: {
+          solid: {
+            body: 'bg-blue-500'
+          }
+        }
       }
     },
     colors: {
--- app/pages/playground/styling-slots.vue
+++ app/pages/playground/styling-slots.vue
@@ -1,6 +1,10 @@
 <template>
   <div class=" p-10">
-    <UCard class="bg-green-500" :ui="{ header: 'bg-green-500' }">
+    <UCard
+      class="bg-green-500"
+      :ui="{ header: 'bg-green-500' }"
+      variant="solid"
+    >
       <template #header>
         <h1>How to Style Component Slots</h1>
       </template>
